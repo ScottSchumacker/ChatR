e600e30d7ce8c76a04287f4277e8a5c46790411e
Scott Schumacker <scottschumacker@Scotts-MacBook-Pro.local>
2021-11-20 11:24:42 -0600

    Adding database

--- ChatR/app.R
+++ ChatR/app.R
@@ -3,11 +3,18 @@
 
 library(shiny)
 library(firebase)
+library(googlesheets4)
+library(rlang)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     "Hello this is a chat application experiment with R",
+    br(),
+    br(),
+    br(),
+    br(),
+    tableOutput('table'),
     br(),
     br(),
     br(),
@@ -24,6 +31,12 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    gs4_deauth()
+    ss <- "https://docs.google.com/spreadsheets/d/1BCcmhgfx_F1bzIi2t3kylu7i92udZIahZ19iReSHC38/edit?usp=sharing"
+    dat <- read_sheet(ss)
+    
+    output$table <- renderTable({ dat })
     
     observeEvent(input$button, {
         output$text <- renderText({ input$Message })
