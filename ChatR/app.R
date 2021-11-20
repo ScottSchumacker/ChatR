5f1628653d77bee9296b293877c71b5072c0c2e1
Scott Schumacker <scottschumacker@Scotts-MacBook-Pro.local>
2021-11-19 23:29:34 -0600

    Added basic functionality

--- ChatR/app.R
+++ ChatR/app.R
@@ -2,15 +2,32 @@
 # Chat application
 
 library(shiny)
+library(firebase)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
+    "Hello this is a chat application experiment with R",
+    br(),
+    br(),
+    br(),
+    br(),
+    textOutput("text"),
+    br(),
+    br(),
+    br(),
+    br(),
+    
+    textInput("Message", NULL, value = "", width = NULL, placeholder = NULL),
+    actionButton("button", "Send Message")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    observeEvent(input$button, {
+        output$text <- renderText({ input$Message })
+    })
 
 }
 
